dee4aacd8ead050cf469d6bf05a9c3e2ca6a4af5
Christian-Hummel <23imc10881@fh-krems.ac.at>
2025-01-30 10:32:33 +0100

    add basic fork process

--- shell.c
+++ shell.c
@@ -9,5 +9,21 @@
 
 int main(int argc, char *argv[])
 {
+
+        int id = fork();
+
+    if (id < 0) {
+        fprintf(stderr, "fork failed\n");
+    } else if (id == 0) {
+
+        printf("Child process\n");
+
+    } else {
+        wait(NULL);
+        printf("Parent procees\n");
+        
+
+    }
+
     return 0;
 }
