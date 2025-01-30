ab8079bf30cd732239b1af7928d133161dc9de64
Christian-Hummel <115420636+Christian-Hummel@users.noreply.github.com>
2025-01-30 10:22:52 +0100

    Create shell.c

---
+++ shell.c
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
