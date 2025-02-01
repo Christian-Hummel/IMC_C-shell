a43fc43dc0e07628c1003775a6cafcbf5b7c2427
Christian-Hummel <23imc10881@fh-krems.ac.at>
2025-02-01 15:22:59 +0100

    moved parsing logic outside of child process and reset flags in parent process properly

--- shell.c
+++ shell.c
@@ -4,14 +4,32 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <signal.h>
 #include <string.h>
 
+void handle_sigchld(){
 
+    pid_t pid;
+    int stat;
+
+    while((pid = waitpid(-1, &stat, WNOHANG)) > 0){
+
+        printf("signal received by process %d\n", pid);
+
+    }
+
+}
 
 int main(){
+    
+    // set up signal handler
+    
+    signal(SIGCHLD, &handle_sigchld);
+
+    
 
     // initialize variables for comparison
-    const char version[] = "version";
+    const char version[] = "globalusage";
     const char execute[] = "exec";
     const char filemod[] = ">";
     const char background[] = "&";
@@ -24,8 +42,9 @@ int main(){
     int sfile = 0;
     int bg = 0;
     
-    // initialize input buffer
+    // initialize input buffer and status variable
     char input[128];
+    int status;
     
     
     while (1){
@@ -40,18 +59,71 @@ int main(){
             // remove newline character at the end
             input[strcspn(input, "\n")] = 0;
 
+            // define pointer to a character array
+            char *argument_array[1024];
+                        
+            // index for argument_array
+            int i = 0;
+
+            // variable for storing filepath for > command 
+            char filepath[128];
+
             char *token = strtok(input, delimiter);
 
-            if (strcmp(quit,token) == 0){
+            if (strcmp(quit,token) == 0 && strtok(NULL, delimiter) == NULL){
                 
                 printf("Exiting...\n");
                 break;
 
-            } else if (strcmp(version,token) ==0) {
+            } else if (strcmp(version,token) ==0 && strtok(NULL, delimiter) == NULL) {
 
                 printf("IMCSH Version 1.0 created by Alexander Berger and Christian Hummel\n");
 
             } else if (strcmp(execute,token) == 0) {
+
+            while(token != NULL){
+
+                if (exe == 0){
+
+                    if(strcmp(execute, token) == 0){
+
+                        printf("Execute command %s\n", token);
+                        exe = 1;
+                        }
+
+                            
+                } else if (exe == 1){
+
+                    if (strcmp(filemod,token) == 0){
+
+                        sfile = 1;
+
+                        token = strtok(NULL, delimiter);
+
+                        strcpy(filepath, token);
+
+                        }
+
+                    else if (strcmp(background,token) == 0){
+
+                        bg = 1;
+                            
+
+                    } else {
+
+                        argument_array[i] = token;
+
+                        i++;
+                            
+                        }
+
+                    }
+
+                        
+                    token = strtok(NULL, delimiter);
+
+                }
+
 
                 // call fork to start child process 
 
@@ -66,60 +138,7 @@ int main(){
                 } else if (pid == 0) {
 
                     printf("Child process\n");
-
-                    // define pointer to a character array
-                    char *argument_array[1024];
-                        
-                    // index for argument_array
-                    int i = 0;
-
-                    // variable for storing filepath for > command 
-                    char filepath[128];
-
-                    while(token != NULL){
-
-                        if (exe == 0){
-
-                            if(strcmp(execute, token) == 0){
-
-                                printf("Execute command %s\n", token);
-                                exe = 1;
-                            }
-
-                            
-                        } else if (exe == 1){
-
-                            if (strcmp(filemod,token) == 0){
-
-                                sfile = 1;
-
-                                token = strtok(NULL, delimiter);
-
-                                strcpy(filepath, token);
-
-                            }
-
-                            else if (strcmp(background,token) == 0){
-
-                                bg = 1;
-                            
-
-                            } else {
-
-                                argument_array[i] = token;
-
-                                i++;
-                            
-                            }
-
-                        }
-
-                        
-                        token = strtok(NULL, delimiter);
-
-                    }
-
-                    
+            
                     //execute program with > modifier
                     //run program in background with & modifier
                     //execute program without modifiers
@@ -156,20 +175,35 @@ int main(){
 
                 } else {
 
+                    waitpid(pid, &status, 0);
+
+
                     if (sfile == 1){
 
                         wait(NULL);
+
                         printf("Process finished with ID %d\n", pid);
+
+                        exe = 0;
+                        sfile = 0;
+                        
 
                     } else if (bg == 1){
 
                         printf("No wait call needed\n");
                         printf("Process finished with ID %d\n", pid);
+
+
+                        exe = 0;
+                        bg = 0;
                             
                     } else {
 
                         wait(NULL);
-                        printf("Process finished with ID %d\n", pid);
+
+                        printf("Process finished regularly with ID %d\n", pid);
+
+                        exe = 0;
 
                     }
 
@@ -177,7 +211,7 @@ int main(){
                 }       
                   
             } else {
-                printf("Inserted %s\n", input);
+                printf("Error: command %s not found\n", input);
             }
 
         }
